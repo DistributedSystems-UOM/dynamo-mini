337129f567dbcdbee9981d370118ffd49ccd1b10
gihankarunarathne <gckarunarathne@gmail.com>
2014-02-20 04:44:15 +0530

    Getting list of nodes which are originally responsible for storing particular key range of given virtual node's replicas implemented

--- src/main/java/akka/dynamo_mini/node_management/ConsistentHash.java
+++ src/main/java/akka/dynamo_mini/node_management/ConsistentHash.java
@@ -71,7 +71,7 @@ public class ConsistentHash<T> {
             Map.Entry m = (Map.Entry) i.next();
             preferenceList.add((T) m.getValue());
         }
-        if (s.size() < numberOfReplicas){
+        if (s.size() < numberOfReplicas) {
             s = ring.entrySet();
             i = s.iterator();
             while (i.hasNext() && cnt++ < numberOfReplicas) {
@@ -79,7 +79,44 @@ public class ConsistentHash<T> {
                 preferenceList.add((T) m.getValue());
             }
         }
-        // printRing();
+        //printRing();
+        return preferenceList;
+    }
+
+    /**
+     * Get the previous K number of nodes which are the original virtual nodes
+     * responsible for storing replica values of current node.
+     *
+     * @param key
+     * @return
+     */
+    public ArrayList<T> getPreviousList(Object key) {
+        if (ring.isEmpty()) {
+            return null;
+        }
+        int hash = hashFunction.hash(key);
+        ArrayList<T> preferenceList = new ArrayList<>();
+
+        ArrayList l = new ArrayList();
+        l.addAll(ring.headMap(hash).values());
+        Collections.reverse(l);
+
+        int cnt = 0, j = 0;
+        // System.out.println("### replicas " + numberOfReplicas);
+        while ( cnt<l.size() && cnt++ < numberOfReplicas) {
+            preferenceList.add( (T) l.get(j++));
+        }
+        if (l.size() < numberOfReplicas) {
+            l.clear();
+            l.addAll(ring.headMap(ring.lastKey()).values());
+            l.add(ring.get(ring.lastKey()));
+            Collections.reverse(l);
+            j = 0;
+            while (j <l.size() && cnt++ < numberOfReplicas) {
+                preferenceList.add( (T) l.get(j++));
+            }
+        }
+        printRing();
         return preferenceList;
     }
 
@@ -87,6 +124,7 @@ public class ConsistentHash<T> {
      * Iterate though the ring and print all elements.
      */
     private void printRing() {
+        System.out.println("Print the ring ... ");
         Set s = ring.entrySet();
         Iterator i = s.iterator();
         while (i.hasNext()) {
